e65d186b32db754607b199967a8e2aa862a3d52b
khoi <khoimap2307@gmail.com>
2025-01-02 15:13:15 +0700

    handle-event2

---
+++ Blazer/Blazor_24/blazor_web_server/Viewmodels/PersonViewModel.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace blazor_web_server.ViewModels;
+
+class PersonViewModel
+{
+    public int Id { get; set; } = 2;
+    public string Name { get; set; } = "ABC";
+    public int Age { get; set; } = 18;
+    public string Image{ get; set; } = "";
+    public PersonViewModel()
+    {
+        Image = $"https://i.pravatar.cc?u={Id}";
+    }
+
+    public PersonViewModel(int newId, string newName, int newAge)
+    {
+        Id = newId;
+        Name = newName;
+        Age = newAge;
+        Image = $"https://i.pravatar.cc?u={Id}";
+    }
+}

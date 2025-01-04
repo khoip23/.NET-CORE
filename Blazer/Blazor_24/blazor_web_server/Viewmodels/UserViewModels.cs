690f5948fce75e62a1ad34750f59e7ea6ee08a18
khoi <khoimap2307@gmail.com>
2025-01-04 16:28:07 +0700

    TwoWay-Binding

---
+++ Blazer/Blazor_24/blazor_web_server/Viewmodels/UserViewModels.cs
@@ -0,0 +1,10 @@
+namespace blazor_web_server;
+
+public class UserViewModels
+{
+    public string UserName { get; set;}
+    public string Password { get; set;}
+    public string FullName { get; set;}
+    public string Gender { get; set;}
+    public string Country { get; set;}
+}

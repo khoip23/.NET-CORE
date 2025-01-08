4f3885cea43ddfc88bb3ddce268900be1eb0094f
khoi <khoimap2307@gmail.com>
2025-01-08 15:41:06 +0700

    component lifting stateup

---
+++ Blazer/Blazor_24/blazor_web_server/Viewmodels/ProductCarVM.cs
@@ -0,0 +1,8 @@
+
+
+public class ProductCarVM{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Image { get; set; }
+}

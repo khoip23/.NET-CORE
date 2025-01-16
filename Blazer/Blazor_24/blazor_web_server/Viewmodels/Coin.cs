4203ae4e592aaf11c9cba456509dfd233805ab3d
khoi <khoimap2307@gmail.com>
2025-01-16 12:05:54 +0700

    cryto-fa

--- Blazer/Blazor_24/blazor_web_server/Viewmodels/Coin.cs
+++ Blazer/Blazor_24/blazor_web_server/Viewmodels/Coin.cs
@@ -1,7 +1,6 @@
-public class Coin
-{
-    public string id { get; set; }
-    public string name { get; set; }
-    public double current_price { get; set; }
-    public string image { get; set; }
+public class Coin{
+    public string id {get;set;}
+    public string name {get;set;}
+    public double current_price {get;set;}
+    public string image {get;set;}
 }

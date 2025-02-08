8614822bfd31f3e1865c6eafab1b270191cbafdd
khoi <khoimap2307@gmail.com>
2025-02-08 13:17:53 +0700

    .

--- CSharp_basic/buoi11_OOP/buoi11_OOP/Models/NhanVien.cs
+++ CSharp_basic/buoi11_OOP/buoi11_OOP/Models/NhanVien.cs
@@ -1,19 +1,15 @@
 public class NhanVien
 {
-
     //static thuộc class chứ không thuộc instance
     //(object đợc tạo ra từ class)
-    public string? maNhanVien{ get;set; }
-    public string? tenNhanVien{ get;set; }
+    public string? maNhanVien { get; set; }
+    public string? tenNhanVien { get; set; }
 
+    public double luongCoBan { get; set; }
 
-    public double luongCoBan{ get;set; }
+    public double soGioLam { get; set; }
 
-
-    public double soGioLam{ get;set; }
-
-
-    public double heSoLuong { get;set; } = 1;
+    public double heSoLuong { get; set; } = 1;
 
     // public NhanVien() //hàm khởi tạo(constructor)
     // {
@@ -33,20 +29,19 @@ public class NhanVien
         }
     }
 
-    public NhanVien(string maNV, string tenNV, double luong, double GioLam )
+    public NhanVien(string maNV, string tenNV, double luong, double GioLam)
     {
         maNhanVien = maNV;
         tenNhanVien = tenNV;
         luongCoBan = luong;
         soGioLam = GioLam;
-
     }
 
     public void nhapThongTinSinhVien()
     {
         System.Console.WriteLine("Nhập mã nv:");
         maNhanVien = Console.ReadLine();
-        
+
         System.Console.WriteLine("Nhập tên nv:");
         tenNhanVien = Console.ReadLine();
 
@@ -62,18 +57,19 @@ public class NhanVien
 
     public double tinhLuong()
     {
-
         return luongCoBan * soGioLam * heSoLuong;
     }
 
     public void xuatThongTinNhanVien()
     {
-        System.Console.WriteLine($@"
+        System.Console.WriteLine(
+            $@"
             Mã nhân viên: {maNhanVien}
             Tên nhân viên: {tenNhanVien}
             Tổng lương: {tinhLuong()}
         
-        ");
+        "
+        );
     }
 
     //static method không thuộc về đối tượng chỉ thuộc về class
